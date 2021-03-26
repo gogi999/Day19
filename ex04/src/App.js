b44f22f24e12d12d1b579d31520553151d10f307
gogi999 <g.petrovic9@gmail>
2021-03-26 16:13:51 +0100

    Added ex04 folder of Day19

---
+++ ex04/src/App.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+      return (
+          <div className="App">
+              <h1>Hello, Padawans!</h1>
+          </div>
+      )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
